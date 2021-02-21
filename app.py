ddc5566e5362125e1ddb31fc4e4da73a96019417
David Gitman <wereking@gmail.com>
2021-02-21 20:16:26 +0200

    added hostip and name in flask

--- app.py
+++ app.py
@@ -5,9 +5,20 @@ import socket
 app = Flask(__name__)
 
 
+@app.route('/')
+def readme():
+    return """ <center><b><p style="font-family:Tahoma"; color:red; font-size: 20px>Welcome to my first flask app!<br/>
+               You can check the host name and IP by going into /host<br/>
+               In order to stop the server you can shut it down by entering /stop</center></b></p>"""
+
+
 @app.route("/host")
 def hostname():
-    return socket.gethostname()
+    host = socket.gethostname()
+    host2 = socket.gethostbyname(host)
+    return " <center><b><p style='font-family:Tahoma';>Host Name:  </b>" + host + "<center><b><p style='font-family:Tahoma';>Host IP:  </b>" + host2
+
+
 
 
 def shutdown_server():
