87a4c22615f654db7d3143eea112e55e4622d9c9
David Gitman <wereking@gmail.com>
2021-02-21 18:51:55 +0200

    added shutdown section

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 from flask import Flask
+from flask import request
 import socket
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/host")
 def hostname():
     return socket.gethostname()
 
 
-@app.route('/terminate')
-def terminate():
-    import os
-    os.system("killall -KILL python")
-    return
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+
+@app.route('/stop')
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
