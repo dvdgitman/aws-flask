9a362e0b737f1bb2ecb48d0099833c590d927dc1
David Gitman <wereking@gmail.com>
2021-02-21 18:36:36 +0200

    terminate

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ app = Flask(__name__)
 @app.route("/")
 def hostname():
     return socket.gethostname()
+
+
+@app.route('/terminate')
+def terminate():
+    import os
+    os.system("killall -KILL python")
+    return
